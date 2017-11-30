d2209143cba3f36945a3c28ab2b7d8bde9a1b3a3
yanghua <879651110@qq.com>
2017-11-30 19:41:48 +0800

    first demo for duotai

---
+++ src/main/java/com/young/thinking/java/DuoTaiDemo.java
@@ -0,0 +1,50 @@
+package com.young.thinking.java;
+
+/**
+ * Created by yanghua on 2017年11月30日
+ *  java多态的例子
+ */
+public class DuoTaiDemo {
+	
+	static void Drive(LearnDrive learnDrive){
+		learnDrive.Drive();
+	}
+	
+	public static void main(String[] args) {
+		DriverTrain dt=new DriverTrain();
+		DriverCar dc=new DriverCar();
+		DriverAirplane da=new DriverAirplane();
+		Drive(dt);
+		Drive(dc);
+		Drive(da);
+	}
+}
+
+	class LearnDrive {
+		void Drive() {
+		};
+	}
+
+	class DriverTrain extends LearnDrive {
+	
+		@Override
+		void Drive() {
+			System.out.println(this.getClass().getSimpleName()+ " 开火车");
+		}
+	}
+
+	class DriverCar extends LearnDrive {
+	
+		@Override
+		void Drive() {
+			System.out.println(this.getClass().getSimpleName()+" 开汽车");
+		}
+	}
+
+	class DriverAirplane extends LearnDrive {
+	
+		@Override
+		void Drive() {
+			System.out.println(this.getClass().getSimpleName()+ " 开飞机");
+		}
+	}
--- src/main/java/com/young/thinking/java/PolymorphismDemo.java
+++
@@ -1,9 +0,0 @@
-package com.young.thinking.java;
-
-/**
- * Created by yanghua on 2017年11月30日
- * java多态的例子
- */
-public class PolymorphismDemo {
-
-}
